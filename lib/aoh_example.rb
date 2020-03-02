4ab47fbb52dc36590b6dd9f2c86c8394ae8d8cd7
Craig Storer <craigstorer@gmail.com>
2020-03-02 02:29:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  hash =  [DON_G.sort,JOELLE_VD.sort,PAT_M.sort,KATE_G.sort,BRUCE_G.sort]
+  hash =  [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
   
 end
 
